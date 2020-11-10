fb75772319ee51a8aa11809cab9ab4c22164e7a9
deathstars22 <josgabfer@gmail.com>
2020-11-09 20:47:48 -0600

    Added footer and icons, images and more

---
+++ Resources/Pages/footer.php
@@ -0,0 +1,9 @@
+<footer class="page-footer">
+    <div class="footer_bg container-fluid text-center text-md-left">
+        <div class="row">
+            <div class="col-md6 mt-md-0 mt-3">
+                
+            </div>
+        </div>
+    </div>
+</footer>
--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ALFAGADA</title>
-    <link rel="stylesheet" href="/Styles/styles.css">
+    <link rel="stylesheet" href="Resources/Styles/styles.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
 
@@ -13,6 +13,10 @@
 
 <!-- Top Bar Section (Search)-->
 <?php include 'Resources/Pages/topBar.php';?>
+
+
+<!-- Footer-->
+<?php include 'Resources/Pages/footer.php';?>
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>

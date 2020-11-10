0c554a19094da8c16a5349153081800e20fc1443
deathstars22 <josgabfer@gmail.com>
2020-11-09 22:10:43 -0600

    Added Copyright

--- Resources/Pages/footer.php
+++ Resources/Pages/footer.php
@@ -1,8 +1,15 @@
-<footer class="page-footer">
-    <div class="footer_bg container-fluid text-center text-md-left">
-        <div class="row">
+<footer class="section footer">
+    <div class="container">
+        <div class="row row-30">
             <div class="col-md6 mt-md-0 mt-3">
-                
+                <p>Vendemos Abarrotes Chinos</p>
+            </div>
+        </div>
+        <div class="bottom container">
+            <div class="pr-xl-4">
+                <p class="rights">
+                    <span>@ </span><span class="copyright-year">2020 </span><span>GoodSoft.com</span><span> All Rights Reserved</span>
+                </p>
             </div>
         </div>
     </div>

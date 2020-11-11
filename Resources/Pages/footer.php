548de80059b2b733a9007ddcc4bf28ce74627cb0
deathstars22 <josgabfer@gmail.com>
2020-11-10 20:18:27 -0600

    Added more footer elements

--- Resources/Pages/footer.php
+++ Resources/Pages/footer.php
@@ -1,16 +1,56 @@
 <footer class="section footer">
-    <div class="container">
-        <div class="row row-30">
-            <div class="col-md6 mt-md-0 mt-3">
-                <p>Vendemos Abarrotes Chinos</p>
+    <div class="before-footer bottom solid-line">
+        <div class="container-lg my-5 mx-2">
+            <div class="row">
+                <div class="col-sm">
+                    <i class="fas fa-laptop-house fa-2x bottom-icons"></i>
+                    <div>
+                        <h2 class="bottom-font">Compra desde la casa!</h2>
+                        <p>Ordena desde la comodidad de tu casa!</p>
+                    </div>
+                </div>
+                <div class="col-sm">
+                <i class="fas fa-dollar-sign fa-2x bottom-icons"></i>
+                    <div>
+                    <h2 class="bottom-font">Precios comodos!</h2>
+                        <p>Todos nuestros productos estan al mejor precio!</p>
+                    </div>
+                </div>
+                <div class="col-sm">
+                <i class="fas fa-clock fa-2x bottom-icons"></i>
+                    <div>
+                    <h2 class="bottom-font">Servicio 24/7</h2>
+                        <p>Estamos disponibles en cuelquier momento!</p>
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="bottom container">
-            <div class="pr-xl-4">
-                <p class="rights">
-                    <span>@ </span><span class="copyright-year">2020 </span><span>GoodSoft.com</span><span> All Rights Reserved</span>
-                </p>
-            </div>
-        </div>
+
     </div>
+        <div class="container my-3">
+            <div class="row">
+                <div class="col-lg-4 cold-md-4">
+                    <div class="footer-info">
+                    <h2 class="bottom-font">Contactanos Ahora!</h2>
+                    <p>Nos gustaria escuchar tus comentarios</p>
+                        <ul>
+                            <li>
+                            <i class="fas fa-map-signs fa-2x bottom-icons"></i>
+                            </li>
+                            <li>
+                            <p> 200 metros norte del palo de mangos</p>
+                            </li>
+                        </ul>
+
+                    </div>
+                </div>
+            </div>
+            <div class="bottom container">
+                <div class="pr-xl-4">
+                    <p class="rights">
+                        <span>@ </span><span class="copyright-year">2020 </span><span>GoodSoft.com</span><span> All Rights Reserved</span>
+                    </p>
+                </div>
+            </div>
+        </div>
 </footer>

06fecd4ef2ba566472b567a9109775b5df1b1ba2
danielcotov <danielcotov@gmail.com>
2020-11-15 20:13:57 -0600

    Changes to links

--- index.php
+++ index.php
@@ -6,13 +6,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>ALFAGADA</title>
     <link rel="stylesheet" href="Resources/Styles/styles.css">
+    <link rel="stylesheet" href="Resources/Styles/bootstrap.css" media="all" type="text/css">
+    <link rel="stylesheet" href="Resources/Styles/styles_top_bar.css" media="all" type="text/css">
+    <link rel="stylesheet" href="Resources/Styles/styles_menu_bar.css" media="all" type="text/css">
     <link rel="stylesheet" href="Resources/Styles/footerStyles.css">
     <link href="https://cdn.lineicons.com/2.0/LineIcons.css" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
     <link href="https://cdn.lineicons.com/2.0/LineIcons.css" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">  
-    
     <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
 </head>
 <body>

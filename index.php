fe693d7e8ff13ee48a7ff5bd977798f3ecc33dcc
danielcotov <danielcotov@gmail.com>
2020-11-15 20:12:33 -0600

    Changes in links

--- index.php
+++ index.php
@@ -5,17 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>ALFAGADA</title>
-<<<<<<< HEAD
-    <link rel="stylesheet" href="Resources/Styles/bootstrap.css" media="all" type="text/css">
-    <link rel="stylesheet" href="Resources/Styles/styles_top_bar.css" media="all" type="text/css">
-    <link rel="stylesheet" href="Resources/Styles/styles_menu_bar.css" media="all" type="text/css">
-    <link rel="stylesheet" href="Resources/Styles/font-awesome.css">
-=======
     <link rel="stylesheet" href="Resources/Styles/styles.css">
     <link rel="stylesheet" href="Resources/Styles/footerStyles.css">
     <link href="https://cdn.lineicons.com/2.0/LineIcons.css" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
->>>>>>> 50792155f7566de20657feac1f7fdc9debf4ceb2
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
     <link href="https://cdn.lineicons.com/2.0/LineIcons.css" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">  

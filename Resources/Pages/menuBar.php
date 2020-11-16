44e6a92601f12acb5d2553af30fd63e599516b60
deathstars22 <josgabfer@gmail.com>
2020-11-15 21:26:55 -0600

    changes

--- Resources/Pages/menuBar.php
+++ Resources/Pages/menuBar.php
@@ -1,16 +1,3 @@
-<!--<div class="menu">
-	<div class="container">
-		<div class="row align-items-center">
-			<div class="col-lg-3 col-md-4 col-sm-8 col-10">
-				<div class="shopby_categories d-none d-xl-block d-lg-block">
-					<a class="shop_category" data-toggle="collapse" href="#myCategories"
-					role="button" aria-expanded="false" aria-controls="myCategories"><i 
-					class="fas fa-bars"></i>&nbsp &nbsp Categorías</a>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>!-->
 <div class="menu">
     <div class="container">
         <div class="row align-item-center">

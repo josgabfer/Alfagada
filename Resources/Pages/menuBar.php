075ba318fa1c9de6f86c55db0017665c385eedb9
deathstars22 <josgabfer@gmail.com>
2020-11-16 21:04:43 -0600

    dropdown

--- Resources/Pages/menuBar.php
+++ Resources/Pages/menuBar.php
@@ -1,15 +1,26 @@
 <div class="menu">
     <div class="container">
         <div class="row align-item-center">
-            <nav class="navbar navbar-default">
+            <nav class="navbar navbar-expand-lg navbar-default">
                 <div class="navbar-collapse" id="bs-megadropdown-tabs">
                     <ul class="nav navbar-nav">
                         <li class="dd active">
                             <a href="index.php" class="act">INICIO</a>
                         </li>
-                        <li class="dd dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">PRODUCTOS
-                            </a>
+                        <li class="dropdown">
+                            <a href="#" class="nav-link dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Productos</a>
+                            <div class="dropdown-menu" id="dropdownbtn" aria-labelledby="navbarDropdown">
+                                <a href="#" id="dropdownitem" class="dropdown-item">Abarrotes</a>
+                                <a href="#" id="dropdownitem" class="dropdown-item">Abarrotes</a>
+                                <a href="#" id="dropdownitem" class="dropdown-item">Abarrotes</a>
+                                <a href="#" id="dropdownitem" class="dropdown-item">Abarrotes</a>
+                                <a href="#" id="dropdownitem" class="dropdown-item">Abarrotes</a>
+                                <a href="#" id="dropdownitem" class="dropdown-item">Abarrotes</a>
+                                <a href="#" id="dropdownitem" class="dropdown-item">Abarrotes</a>
+                                <a href="#" id="dropdownitem" class="dropdown-item">Abarrotes</a>
+                                <a href="#" id="dropdownitem" class="dropdown-item">Abarrotes</a>
+
+                            </div>
                         </li>
                         <li class="dd active">
                             <a href="contact.php" class="act">CONTACTO</a>

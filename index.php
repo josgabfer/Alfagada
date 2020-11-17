6d5ce982bd7bbd087ece59bfb07539b4588fed10
danielcotov <danielcotov@gmail.com>
2020-11-16 19:40:58 -0600

    Slide fixed

--- index.php
+++ index.php
@@ -46,11 +46,11 @@
       <img class="d-block w-100" src="Resources/imgs/navidad.png" alt="banner">
     </div>
   </div>
-  <a class="carousel-control-prev" href="#" role="button" data-slide="prev">
+  <a class="carousel-control-prev" href="#slide" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
   </a>
-  <a class="carousel-control-next" href="#" role="button" data-slide="next">
+  <a class="carousel-control-next" href="#slide" role="button" data-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="sr-only">Next</span>
   </a>

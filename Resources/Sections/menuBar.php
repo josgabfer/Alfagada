fa33129daee0cef0fe604a02f9bc58dd6a37c562
deathstars22 <josgabfer@gmail.com>
2020-11-24 19:18:17 -0600

    almost finished dropdown

--- Resources/Sections/menuBar.php
+++ Resources/Sections/menuBar.php
@@ -1,31 +1,39 @@
-<div class="menu">
-    <div class="container">
-        <div class="row align-item-center">
-            <nav class="navbar navbar-expand-lg navbar-default minav">
-                <div class="navbar-collapse" id="bs-megadropdown-tabs">
-                    <ul class="nav ml-auto navbar-nav">
-                        <li class="dd active">
-                            <a href="index.php" class="act">INICIO</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Productos</a>
-                            <ul class="dropdown-menu navbar-default mimenu" aria-labelledby="navbarDropdown">
-                                <a href="#" class="dropdown-item">Abarrotes</a>
-                                <a href="#" class="dropdown-item">Abarrotes</a>
-                                <a href="#" class="dropdown-item">Abarrotes</a>
-                                <a href="#" class="dropdown-item">Abarrotes</a>
-                            </ul>
-                        </li>
-                        <li class="dd active">
-                            <a href="#contact.php" class="act">CONTACTO</a>
-                        </li>
-                        <li class="dd active">
-                            <a href="ubicacion.php" class="act">UBICACION</a>
-                        </li>
-                    
-                    </ul>
-                </div>
-            </nav>
+<div class="container-fluid p-0">
+    <nav class="navbar navbar-expand-lg my-navbar">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav m-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">Categorias</a>
+              <ul class="dropdown-menu mega-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Abarrotes</a>
+                <a class="dropdown-item" href="#">Bebidas</a>
+                <a class="dropdown-item" href="#">Carnes</a>
+                <a class="dropdown-item" href="#">Refrigerados</a>
+                <a class="dropdown-item" href="#">Frutas y Verduras</a>
+                <a class="dropdown-item" href="#">Lacteos</a>
+                <a class="dropdown-item" href="#">Panaderia y reposteria</a>
+              </ul>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">Contactenos</a>
+              <ul class="dropdown-menu mega-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Sobre nosotros</a>
+                <a class="dropdown-item" href="#">Nuestra Compañia</a>
+                <a class="dropdown-item" href="#">Contacto</a>
+                <a class="dropdown-item" href="#">Sucursales</a>
+              </ul>
+            </li>
+            <li class="nav-item ">
+              <a class="nav-link" href="#">A domicilio</a>
+            </li>
+        </ul>
         </div>
-    </div>
-</div>
+      </nav>
+   </div>

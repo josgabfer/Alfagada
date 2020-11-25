d8674453f71ec0b64b7a6a3c6f5e48a98d6c06b3
deathstars22 <josgabfer@gmail.com>
2020-11-24 19:20:04 -0600

    menubar

--- Resources/Sections/menuBar.php
+++ Resources/Sections/menuBar.php
@@ -1,39 +1,3 @@
-<<<<<<< HEAD
-<div class="menu">
-    <div class="container">
-        <div class="row align-item-center">
-            <nav class="navbar navbar-expand-lg navbar-default">
-                <div class="navbar-collapse" id="bs-megadropdown-tabs">
-                    <ul class="nav navbar-nav">
-                        <li class="dd active">
-                            <a href="index.php" class="act">INICIO</a>
-                        </li>
-                        <li class="dropdown">
-                            <a href="#" class="nav-link dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Productos</a>
-                            <div class="dropdown-menu" id="dropdownbtn" aria-labelledby="navbarDropdown">
-                                <a href="#" id="dropdownitem" class="dropdown-item">Abarrotes</a>
-                                <a href="#" id="dropdownitem" class="dropdown-item">Abarrotes</a>
-                                <a href="#" id="dropdownitem" class="dropdown-item">Abarrotes</a>
-                                <a href="#" id="dropdownitem" class="dropdown-item">Abarrotes</a>
-                                <a href="#" id="dropdownitem" class="dropdown-item">Abarrotes</a>
-                                <a href="#" id="dropdownitem" class="dropdown-item">Abarrotes</a>
-                                <a href="#" id="dropdownitem" class="dropdown-item">Abarrotes</a>
-                                <a href="#" id="dropdownitem" class="dropdown-item">Abarrotes</a>
-                                <a href="#" id="dropdownitem" class="dropdown-item">Abarrotes</a>
-
-                            </div>
-                        </li>
-                        <li class="dd active">
-                            <a href="#contact.php" class="act">CONTACTO</a>
-                        </li>
-                        <li class="dd active">
-                            <a href="ubicacion.php" class="act">UBICACION</a>
-                        </li>
-                    
-                    </ul>
-                </div>
-            </nav>
-=======
 <div class="container-fluid p-0">
     <nav class="navbar navbar-expand-lg my-navbar">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -70,7 +34,6 @@
               <a class="nav-link" href="#">A domicilio</a>
             </li>
         </ul>
->>>>>>> origin/JGD
         </div>
       </nav>
    </div>

7c282ccb368395a2e63e2950d4a0f6cb8df737ee
danielcotov <danielcotov@gmail.com>
2020-12-09 17:19:34 -0600

    .

--- Resources/Sections/menuBar.php
+++ Resources/Sections/menuBar.php
@@ -5,33 +5,34 @@
       
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav">
-          <li class="nav-item active ">
+            <li class="nav-item active ">
               <a class="nav-link" href="index.php">Inicio</a>
             </li>
             <li class="nav-item active dropdown">
-              <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">Categorías</a>
-              <ul class="dropdown-menu mega-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="producto.php">Abarrotes</a>
-                <a class="dropdown-item" href="#">Bebidas</a>
-                <a class="dropdown-item" href="#">Carnes</a>
-                <a class="dropdown-item" href="#">Refrigerados</a>
-                <a class="dropdown-item" href="#">Frutas y Verduras</a>
-                <a class="dropdown-item" href="#">Lacteos</a>
-                <a class="dropdown-item" href="#">Panaderia y reposteria</a>
-              </ul>
+                <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">Categorías</a>
+                <ul class="dropdown-menu mega-menu" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item" href="producto.php">Abarrotes</a>
+                  <a class="dropdown-item" href="#">Bebidas</a>
+                  <a class="dropdown-item" href="#">Carnes</a>
+                  <a class="dropdown-item" href="#">Refrigerados</a>
+                  <a class="dropdown-item" href="#">Frutas y Verduras</a>
+                  <a class="dropdown-item" href="#">Lacteos</a>
+                  <a class="dropdown-item" href="#">Panaderia y reposteria</a>
+                </ul>
             </li>
             <li class="nav-item active dropdown">
-              <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">Contáctenos</a>
-              <ul class="dropdown-menu mega-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="#">Sobre nosotros</a>
-                <a class="dropdown-item" href="#">Nuestra Compañia</a>
-                <a class="dropdown-item" href="#">Contacto</a>
-                <a class="dropdown-item" href="#">Sucursales</a>
-              </ul>
+                <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">Contáctenos</a>
+                <ul class="dropdown-menu mega-menu" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item" href="#">Sobre nosotros</a>
+                  <a class="dropdown-item" href="#">Nuestra Compañia</a>
+                  <a class="dropdown-item" href="#">Contacto</a>
+                  <a class="dropdown-item" href="#">Sucursales</a>
+                </ul>
             </li>
-            <li class="nav-item active ">
-              <a class="nav-link" href="ubicacion.php">Ubicación</a>
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Ubicación</a>
             </li>
-        </ul>
+            
+          </ul>
         </div>
       </nav>

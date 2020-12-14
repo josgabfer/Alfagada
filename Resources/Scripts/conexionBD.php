9083325f2c4db9588d1aff03e6d6f401624b0a0f
Fabián Bolaños <>
2020-12-14 01:56:03 -0600

    Updated conexion

--- Resources/Scripts/conexionBD.php
+++ Resources/Scripts/conexionBD.php
@@ -2,10 +2,27 @@
 
     function OpenCon()
     {
-        $servidor = "MYSQLCONNSTR_localdb";
+        $servidor = "";
+        $usuario = "";
+        $password = "";
+        $baseDatos = "";
+
+        // Parsing connnection string
+        foreach ($_SERVER as $key => $value) {
+            if (strpos($key, "MYSQLCONNSTR_") !== 0) {
+                continue;
+            }
+            
+            $servidor = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+            $baseDatos = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+            $usuario = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+            $password = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+        }
+        /*$servidor = "MYSQLCONNSTR_localdb";
         $usuario = "root";
         $password = "";
         $baseDatos = "alfagada";
+        */
         $conn = new mysqli($servidor,$usuario,$password,$baseDatos) or die("Connect failed:" . $conn -> error);
         return $conn;
     }

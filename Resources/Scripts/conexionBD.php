9eb202ee8d654678ba0550ea8d75c1f8da4c1b2e
deathstars22 <josgabfer@gmail.com>
2020-12-16 15:13:09 -0600

    bitnami connection stuff

--- Resources/Scripts/conexionBD.php
+++ Resources/Scripts/conexionBD.php
@@ -2,26 +2,11 @@
 
     function OpenCon()
     {
-        $connectstr_dbhost = '';
-        $connectstr_dbname = '';
-        $connectstr_dbusername = '';
-        $connectstr_dbpassword = '';
-
-        foreach ($_SERVER as $key => $value) {
-            if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
-                continue;
-            }
-            echo $value;
-            $servidor = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
-            $usuario = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
-            $password = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
-            $baseDatos = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
-}
         
-        /*$servidor = "localhost";
+        $servidor = "localhost:80";
         $usuario = "root";
-        $password = "";
-        $baseDatos = "alfagada";*/
+        $password = "D49WKj3LRneh";
+        $baseDatos = "alfagada";
         
         $conn = new mysqli($servidor,$usuario,$password,$baseDatos) or die("Connect failed:" . $conn -> error);
         return $conn;

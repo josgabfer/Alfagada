6d8da4e0d01ec91da467c6a7ebdcc774e0ef9cad
deathstars22 <josgabfer@gmail.com>
2020-12-16 16:16:15 -0600

    working creds

--- Resources/Scripts/conexionBD.php
+++ Resources/Scripts/conexionBD.php
@@ -3,9 +3,9 @@
     function OpenCon()
     {
         
-        $servidor = "127.0.0.1";
-        $usuario = "alfagada";
-        $password = "alfagada";
+        $servidor = "localhost";
+        $usuario = "root";
+        $password = "D49WKj3LRneh";
         $baseDatos = "alfagada";
         
         $conn = new mysqli($servidor,$usuario,$password,$baseDatos) or die("Connect failed:" . $conn -> error);
